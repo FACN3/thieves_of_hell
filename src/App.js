3216debbf6dac11f28ede6e6a8620889d9116796
hoslack <hoslackochieng@gmail.com>
2018-01-29 17:24:52 +0200

    Added form and list component, can be able to add and delete todos relates #1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+import Form from './components/Form';
+import ItemList from './components/ItemList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  addTodo(text) {
+    const addtime = Date.now();
+    const newTodo = { text, addtime };
+    console.log(newTodo);
+    const newState = this.state.items.concat([newTodo]);
+    this.setState({ items: newState });
+  }
+
+  deleteTodo(id) {
+    const newState = this.state.items.filter(item => {
+      return item.addtime != id;
+    });
+    this.setState({ items: newState });
+  }
+
   render() {
-    return <div className="App">Hello world</div>;
+    return (
+      <div className="container">
+        <h3>Welcome</h3>
+        <Form addTodo={this.addTodo} />
+
+        <ul className="collection">
+          <ItemList deleteTodo={this.deleteTodo} items={this.state.items} />
+        </ul>
+      </div>
+    );
   }
 }
 
