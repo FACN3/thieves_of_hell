78b7b2c6a1b630e3ec3b084d0bdb73ef85502bca
hoslack <hoslackochieng@gmail.com>
2018-01-30 13:02:19 +0200

    Added sorting priority with colours and designed with materializes relates #1

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,16 +4,22 @@ const Form = props => {
   const handleSubmit = event => {
     event.preventDefault();
     const text = event.target.text.value;
-    props.addTodo(text);
+    const priority = event.target.priority.value;
+    props.addTodo(text, priority);
     event.target.text.value = '';
+    event.target.priority.value = '';
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div class="input-field col s6">
-          <input id="text" name="text" type="text" className="validate" />
-          <label for="text">Enter todo Item</label>
+        <div className="input-field col s6">
+          <input id="text" name="text" type="text" maxlength="40" required />
+          <label htmlFor="text">Enter a To Do Item</label>
+        </div>
+        <div className="input-field col s6">
+          <input id="priority" name="priority" type="number" min="1" max="5" required />
+          <label htmlFor="priority">Priority(1 important - 5 less important)</label>
         </div>
         <button type="submit" className="btn">
           Submit
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,20 +2,26 @@ import React from 'react';
 
 const ItemList = props => {
   const onDelete = event => {
-    console.log(event.target.parentNode.id);
-    const id = event.target.parentNode.id;
+    const id = event.target.parentNode.parentNode.parentNode.id;
     props.deleteTodo(id);
   };
 
   return (
     <div>
       {props.items.map(item => {
+        const className = `collection-item hoverable card-panel blue lighten-${item.priority}`;
         return (
-          <li key={item.addtime} id={item.addtime} className="collection-item">
-            {item.text}
-            <button onClick={onDelete} className="btn red right waves-effect waves-light">
-              <i className="material-icons">delete</i>
-            </button>
+          <li key={item.addtime} id={item.addtime} className={className}>
+            <div className="row">
+              <div className="col s10 truncate ">
+                <p className="truncate">{item.text}</p>
+              </div>
+              <div className="col s2">
+                <button onClick={onDelete} className="btn red right waves-effect waves-light">
+                  <i className="material-icons">delete</i>
+                </button>
+              </div>
+            </div>
           </li>
         );
       })}
